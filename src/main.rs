0b56f86e7d91e8cb49f6054980d918d1ef5e0ac0
Eamonn <eamonn.m.r@gmail.com>
2016-11-23 11:35:29 -0500

    Still fighting with the borrow checker
    Adds method to create list structures from tokens.
    
    Still not compiling because the borrow checker isn't satisfied.

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,44 @@ fn main() {
 }
 
 fn parse_line (source: String) -> ParseTreeNode {
-    let mut root_node = ParseTreeNode::List(Vec::<ParseTreeNode>::new());
-    for token in source.replace("(", " ( ").replace(")", " ) ").split(" "){
-        println!("{}", token );
+    //fn parse_list<'token_iter, I>(vals: I) -> ParseTreeNode
+    //        where I: Iterator<Item=String>
+    //{
+    fn parse_list<'a>( token_iter: std::str::Split<'a, &str> ) -> ParseTreeNode {
+        let mut node = ParseTreeNode::List(Vec::<ParseTreeNode>::new());
+        match node {
+            ParseTreeNode::List(ref mut list) => {
+	        	loop {
+    		        let mut token_option = token_iter.next();
+
+                    match token_option {
+                        None => break, // TODO: Crash - expecting close paren
+
+                        Some( token ) => {
+							if token == " " {
+                        		continue;
+                    		} else if token == " ( " {
+                        		list.push( parse_list( token_iter ) );
+                    		} else if token == " ) " {
+                        		break;
+                        	} else {
+		                        list.push( ParseTreeNode::Symbol( token.to_string() ));
+        		            }
+
+						}
+                    }
+        		}
+            }
+            _ => ()
+
+        }
+
+        return node;
     }
-    return root_node;
+
+    let space_added_source = source.replace("(", " ( ").replace(")", " ) ");
+    // https://github.com/kballard/rfcs/blob/2d3ff42b821ab80bd6a7b3b8fda0e1c238cc7de0/active/0000-better-temporary-lifetimes.md
+    let mut tokens = space_added_source.split(" ");
+
+    return parse_list( tokens );
 }
