83683a6834b7d95a8693c561a4cd884f4d8f436e
EamonnMR <Eamonn.m.r@gmail.com>
2020-09-05 10:26:43 -0400

    Pulls scope out into its own file
    Sadly everything needs to be pub'd

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,14 @@ use std::iter::Iterator;
 
 mod node;
 mod parse;
+mod scope;
 
 pub use crate::node::ParseTreeNode;
 pub use crate::parse::parse_line;
+pub use crate::scope::Scope;
+pub use crate::scope::get;
+pub use crate::scope::set;
 
-struct Scope {
-    parent: Option<Box<Scope>>,
-    locals: HashMap<String, ParseTreeNode>,
-}
-
-fn get(scope: &Scope, key: &String) -> ParseTreeNode {
-    // gets a node from the scope, or Nil if it is not found.
-    match scope.locals.get(key) {
-        Some(node) => {
-            return node.to_owned();
-        }
-        None  => {
-            match scope.parent {
-                Some(ref parent) => {
-                    return get(&parent, key);
-                }
-                None => {
-                    // bad bad very not good
-                    // we need better nil handling
-                    return ParseTreeNode::Nil;
-                }
-            }
-        }
-    }
-}
-
-fn set(scope: &mut Scope, key: String, value: ParseTreeNode){
-    scope.locals.insert(key, value);
-}
 
 fn expect_list(node: ParseTreeNode) -> Vec<ParseTreeNode> {
     match node {
---
+++ src/scope.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+use std::iter::Iterator;
+
+
+pub use crate::node::ParseTreeNode;
+pub use crate::parse::parse_line;
+
+pub struct Scope {
+    pub parent: Option<Box<Scope>>,
+    pub locals: HashMap<String, ParseTreeNode>,
+}
+
+pub fn get(scope: &Scope, key: &String) -> ParseTreeNode {
+    // gets a node from the scope, or Nil if it is not found.
+    match scope.locals.get(key) {
+        Some(node) => {
+            return node.to_owned();
+        }
+        None  => {
+            match scope.parent {
+                Some(ref parent) => {
+                    return get(&parent, key);
+                }
+                None => {
+                    // bad bad very not good
+                    // we need better nil handling
+                    return ParseTreeNode::Nil;
+                }
+            }
+        }
+    }
+}
+
+pub fn set(scope: &mut Scope, key: String, value: ParseTreeNode){
+    scope.locals.insert(key, value);
+}
